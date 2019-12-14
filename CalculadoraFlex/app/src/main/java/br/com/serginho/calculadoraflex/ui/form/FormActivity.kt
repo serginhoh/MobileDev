9b2110e8057242e278c59aea5f966a4be251093d
Logon Aluno <logonaluno@local.com>
2019-12-14 09:56:37 -0300

    update

--- CalculadoraFlex/app/src/main/java/br/com/serginho/calculadoraflex/ui/form/FormActivity.kt
+++ CalculadoraFlex/app/src/main/java/br/com/serginho/calculadoraflex/ui/form/FormActivity.kt
@@ -1,13 +1,11 @@
-package br.com.serginho.calculadoraflex.ui.main
+package br.com.serginho.calculadoraflex.ui.form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.serginho.calculadoraflex.R
 
-class MainActivity : AppCompatActivity() {
+class FormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
